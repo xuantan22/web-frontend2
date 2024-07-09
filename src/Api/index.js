654174edce86ef139177d74108ee7b0ddba8a56d
xuantan22 <144461824+xuantan22@users.noreply.github.com>
2024-07-09 12:27:06 +0700

    Update index.js

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'http://localhost:5000'; // Thay thế bằng URL API của bạn
+export const API_BASE_URL = 'https://web-backend-qban.onrender.com'; // Thay thế bằng URL API của bạn
